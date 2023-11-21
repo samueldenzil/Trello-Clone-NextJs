dc85996c63390cfcc0387df75fe4cd85ac401870
samueldenzil <denzil.s.samuel@gmail.com>
2023-11-21 14:25:58 +0530

    Add loading state animation in sidebar

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -5,6 +5,7 @@ import { usePathname, useRouter } from 'next/navigation'
 import { AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export type Organization = {
   id: string
@@ -90,5 +91,16 @@ export function NavItem({ isActive, isExpanded, organization, onExpand }: NavIte
         ))}
       </AccordionContent>
     </AccordionItem>
+  )
+}
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="relative h-10 w-10 shrink-0">
+        <Skeleton className=" h-full w-full" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
   )
 }
